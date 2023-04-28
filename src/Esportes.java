c1a4107b554d8be0ae1c88f0101e53fd6a5aff41
Kaylane <Kaylane@DESKTOP-S9MG05J>
2023-04-28 15:14:01 -0300

    classe abstrata

---
+++ src/Esportes.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Kaylane
+ */
+public abstract class Esportes {
+    
+    private String nome;
+    private int votos;
+
+    public Esportes (String nome) {
+        this.nome = nome;
+        this.votos = 0;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getVotos() {
+        return votos;
+    }
+
+    public void adicionarVoto() {
+        votos++;
+    }
+}

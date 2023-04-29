50d9a5cecbd5e6cd7bba18fb5ec1d71d278e1c75
Kaylane <Kaylane@DESKTOP-S9MG05J>
2023-04-29 10:48:30 -0300

    eleitores

--- src/LerDadosEleitores.java
+++ src/LerDadosEleitores.java
@@ -7,6 +7,29 @@
  *
  * @author Kaylane
  */
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class LerDadosEleitores {
-    
-}
+    public static void main(String[] args) {
+        String nomeArquivo = "eleitores.txt";
+        ArrayList<Eleitor> eleitores = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(nomeArquivo))) {
+            String linha = br.readLine();
+            while (linha != null) {
+                String[] dados = linha.split(";");
+                String nome = dados[0];
+                int titulo = Integer.parseInt(dados[1]);
+                Eleitor eleitor = new Eleitor(nome, titulo);
+                eleitores.add(eleitor);
+                linha = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao ler arquivo " + nomeArquivo);
+            e.printStackTrace();
+        }
+    }

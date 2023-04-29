a725a5b44851b35cc112d232d21dae2b187ab1d8
Kaylane <Kaylane@DESKTOP-S9MG05J>
2023-04-29 17:59:09 -0300

    pacote

--- src/Arquivos/LerDadosEleitores.java
+++ src/Arquivos/LerDadosEleitores.java
@@ -1,4 +1,4 @@
-package interfaces;
+package Arquivos;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
--- src/interfaces/GerarResultados.java
+++ src/interfaces/GerarResultados.java
@@ -1,4 +1,4 @@
-package Objetos;
+package interfaces;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license

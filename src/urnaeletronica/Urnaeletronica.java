ee62e76d5736ed7e1530d21e1895a26469542868
Kaylanegomes18 <kaylanegomes2003@gmail.com>
2023-05-11 14:42:28 -0300

    teste

--- src/urnaeletronica/Urnaeletronica.java
+++ src/urnaeletronica/Urnaeletronica.java
@@ -2,17 +2,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package urnaeletronica;
+package Arquivos;
 import java.util.ArrayList;
 import Objetos.Eleitor;
-import Arquivos.*;
+
 /**
  *
  * @author Vênisson Cardoso
  */
 public class Urnaeletronica {
     public static void main(String[] args) {
-        ArrayList<Eleitor> eleitores = LerDadosEleitores.lerEleitores("eleitores.txt");
+        ArrayList<Eleitor> eleitores = LerDadosEleitores.lerEleitores("./eleitores.txt");
 
         for (Eleitor eleitor : eleitores) {
             System.out.println("Nome: " + eleitor.getNome() + ", Tìtulo: " + eleitor.getTituloEleitor());
